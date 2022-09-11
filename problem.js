1eae0783be60c0a1ea819cb1c6a648024f5aa40a
Dipta-Sarker <contact.diptasarker@gmail.com>
2022-09-11 15:41:23 +0600

    practice problem

---
+++ problem.js
@@ -0,0 +1,43 @@
+// Practice-Problem-1
+
+var mom = 1000;
+console.log(mom);
+var oranges = 300;
+var apples = 400;
+var cost = oranges + apples;
+var have = mom - cost;
+console.log(have);
+
+
+// Practice-Problem-2
+
+var mathematics = 75.25;
+var biology = 65;
+var chemisty = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var total = mathematics + biology + chemisty + physics + bangla;
+var averageMarks = total / 5;
+averageMarks = averageMarks.toFixed(2);
+averageMarks = parseFloat(averageMarks);
+
+console.log(averageMarks);
+
+
+
+// // Practice-Problem-3
+
+var web = "I am going to be";
+var dev = "an awesome web developer";
+
+var webDev = web + " " + dev;
+console.log(webDev)
+
+
+// Practice-Problem-4
+
+var number = 119;
+var divided = 5;
+var get = number % divided;
+console.log(get);
